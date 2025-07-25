0d17a5264b777d6515660e5e159d4d56cd7b6cd6
RohitLonkar3114 <rohitlonkar3114@gmail.com>
2025-07-25 07:57:33 +0530

    Add project files.

---
+++ ProjectForJenkins/Operations.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectForJenkins
+{
+    public class Operations
+    {
+
+        public void Addition()
+        {
+            int a = 10, b = 40, c;
+            c = a + b;
+            Console.WriteLine("Addition = " + c);
+        }
+    }
+}
